82761fcf7b576033486a3bc5597f0d78e3f98d4c
Navaneeth <navaneethk969@gmail.com>
2025-12-30 15:11:43 +0530

    Enables dynamic CORS origin via environment variables

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,13 +1,27 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost", "http://192.168.0.152"}
+
+	// Default Origins
+	origins := []string{
+		"http://localhost:3000", // Nuxt Dev
+		"http://localhost",      // Local Docker Production
+	}
+
+	frontendURL := os.Getenv("FRONTEND_URL")
+	if frontendURL != "" {
+		origins = append(origins, frontendURL)
+	}
+
+	config.AllowOrigins = origins
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
 
